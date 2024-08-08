a7f70603538201208bf962b7f29ff6dc089e95ec
letovDmitry <67585149+letovDmitry@users.noreply.github.com>
2024-08-08 22:57:58 +0600

    payment

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -29,7 +29,7 @@ export class OrderService {
 
     this.ordersGateway.handleEmitNotification()
 
-    return `${payment}`.replace(/<[^>]+>/g, '');
+    return `${payment}`;
 }
 
   async createOrderYookassa(dto: any) {
